50a5deb89b54f3011290b3a0016aa0cc2e3bbc9d
刘志文 <552176426@qq.com>
2020-10-24 20:59:21 +0800

    result

---
+++ src/main/java/com/lhhh/bean/Result.java
@@ -0,0 +1,15 @@
+package com.lhhh.bean;
+
+import lombok.Data;
+
+/**
+ * @author: lhhh
+ * @date: Created in 2020/10/24
+ * @description:
+ * @version:1.0
+ */
+@Data
+public class Result {
+    private Integer code;
+    private Object Data;
+}
